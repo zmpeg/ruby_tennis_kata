e6e243b937cd42a5bc7cd64e9e0d082dfe429cf0
Amir Barylko <amir@barylko.com>
2012-12-05 12:14:57 -0600

    Added players to the game (failing)

--- tennis_game.rb
+++ tennis_game.rb
@@ -1,15 +1,22 @@
 
 class TennisGame
+  attr_reader :players
   
-  def initialize
+  def initialize(players)
+    @players = players
     @scores = {}
+    @score_map = [:love, :fifteen, :thirty, :forty]
   end
-  
+
   def win_the_ball(player)
-    @scores[player] = score(player) +  1
+    @scores[player] = balls_won(player) +  1
   end
   
   def score(player)
+    @score_map[balls_won(player)]
+  end
+  
+  def balls_won(player)
     @scores[player] || 0
   end
   
--- tennis_game_spec.rb
+++ tennis_game_spec.rb
@@ -4,15 +4,19 @@ require_relative './tennis_game'
 
 describe TennisGame do
   
-  let(:players) { Set.new [:red, :blue] }
+  let(:players) { [:red, :blue] }
 
+  subject { TennisGame.new(players) }
+  
+  describe "#players" do
+    its(:players) { should == players }
+  end
+  
   context "First round" do
     [:red, :blue].each do |player|
       context "#{player} scores" do
-        before {subject.win_the_ball(player)} 
-
-        it { subject.score(player).should == 1 }
-        it { subject.score(players.delete(player).first).should == 0 }
+        before { subject.win_the_ball(player) } 
+        it { subject.score.should == :fifteen }
       end
     end
   end
@@ -21,9 +25,8 @@ describe TennisGame do
     [:red, :blue].each do |player|
       context "#{player} scores twice" do
         before { 2.times { subject.win_the_ball(player) } }
-
-        it { subject.score(player).should == 2 }
-        it { subject.score(players.delete(player).first).should == 0 }
+  
+        it { subject.score(player).should == :thirty }
       end
     end
   end
