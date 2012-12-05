6c7c1f8e84d605932c1057002e52751fae36a388
Amir Barylko <amir@barylko.com>
2012-12-05 11:48:29 -0600

    Added test for the second round

--- tennis_game.rb
+++ tennis_game.rb
@@ -1,12 +1,16 @@
 
 class TennisGame
   
+  def initialize
+    @scores = {}
+  end
+  
   def win_the_ball(player)
-    @won_the_ball = player
+    @scores[player] = (@scores[player] && @scores[player] + 1) ||  1
   end
   
   def score(player)
-    player == @won_the_ball ? 1 : 0
+    @scores[player] || 0
   end
   
 end
--- tennis_game_spec.rb
+++ tennis_game_spec.rb
@@ -12,10 +12,20 @@ describe TennisGame do
         before {subject.win_the_ball(player)} 
 
         it { subject.score(player).should == 1 }
-        it { subject.score(@players.delete(player).first).should == 0 }
+        it { subject.score(players.delete(player).first).should == 0 }
       end
     end
   end
     
+  context "Second round" do
+    [:red, :blue].each do |player|
+      context "#{player} scores twice" do
+        before { 2.times { subject.win_the_ball(player) } }
+
+        it { subject.score(player).should == 2 }
+        it { subject.score(players.delete(player).first).should == 0 }
+      end
+    end
+  end
 
 end
